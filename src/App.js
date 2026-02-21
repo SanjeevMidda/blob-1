fd98f4677fb85acd57bd165d153aee42fdb94c75
Sanjeev Midda <s.midda1@gmail.com>
2026-02-21 12:32:37 +0000

    created extra corners for blob

--- src/App.js
+++ src/App.js
@@ -9,21 +9,34 @@ function App() {
   // DONE create function to change blob's corners
   // create interval between each transition
 
-  const randomCornerValue = () => `${Math.floor(Math.random() * 100)}`;
+  const randomCornerValue = () => Math.floor(Math.random() * 100);
 
   const [corners, setCorners] = useState({
-    topLeft: 10,
-    topRight: 10,
-    bottomRight: 10,
-    bottomLeft: 10,
+    topLeftH: 10,
+    topLeftV: 10,
+    topRightH: 10,
+    topRightV: 10,
+
+    bottomRightH: 10,
+    bottomRightV: 10,
+
+    bottomLeftH: 10,
+    bottomLeftV: 10,
   });
 
   const changeBlob = () =>
     setCorners({
-      topLeft: randomCornerValue(),
-      topRight: randomCornerValue(),
-      bottomRight: randomCornerValue(),
-      bottomLeft: randomCornerValue(),
+      topLeftH: randomCornerValue(),
+      topLeftV: randomCornerValue(),
+
+      topRightH: randomCornerValue(),
+      topRightV: randomCornerValue(),
+
+      bottomRightH: randomCornerValue(),
+      bottomRightV: randomCornerValue(),
+
+      bottomLeftH: randomCornerValue(),
+      bottomLeftV: randomCornerValue(),
     });
 
   useEffect(() => {
@@ -34,12 +47,12 @@ function App() {
 
   return (
     <div className="App">
-      <h1>bio.</h1>
+      <h1 className="title">bio.</h1>
       <div
         className="blob"
         onClick={changeBlob}
         style={{
-          borderRadius: `${corners.topLeft}% ${corners.topRight}% ${corners.bottomRight}% ${corners.bottomLeft}%`,
+          borderRadius: `${corners.topLeftH}% ${corners.topRightH}% ${corners.bottomRightH}% ${corners.bottomLeftH}% / ${corners.topLeftV}% ${corners.topRightV}%  ${corners.bottomRightV}% ${corners.bottomLeftV}%`,
         }}
       ></div>
     </div>
