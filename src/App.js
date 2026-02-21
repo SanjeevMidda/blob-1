f9435369d1ca19e209b6802b399bbd2cfda70b0f
Sanjeev Midda <s.midda1@gmail.com>
2026-02-21 10:32:08 +0000

    created blob UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./index.css";
 
 function App() {
-  return <div className="App"></div>;
+  // create a blob which changes its corners every 1 sec
+
+  // create blob
+  // create state to hold dynamic values for blob
+  // create interval between each transition
+
+  return (
+    <div className="App">
+      <div className="blob"></div>
+    </div>
+  );
 }
 
 export default App;
